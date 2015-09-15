2e7df958204027f20727cfe0b7b7fb6e1fc30d69
grimcoder <papochka12>
2015-09-14 23:39:08 -0700

    initial commit

---
+++ ProduceOs/ProduceOs/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ProduceOs
+//
+//  Created by Taras Kovtun on 9/14/15.
+//  Copyright © 2015 Taras Kovtun. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
